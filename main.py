cbc7904363d2723708cb98ac3a54adf9b5d95804
sjbl69 <selma.jbl92@gmail.com>
2026-03-25 11:15:04 +0100

    Add MLflow images to README

--- main.py
+++ main.py
@@ -8,27 +8,24 @@ def main():
     print(" PROJET PRET A DEPENSER")
     print("===================================\n")
 
-    # =========================
     # ETAPE 1 : DATA PREPARATION
-    # =========================
+    
     print("ETAPE 1 - PREPARATION DES DONNEES")
 
     application = prepare_dataset()
 
     print("\nETAPE 1 TERMINÉE\n")
 
-    # =========================
     # ETAPE 2 : MODEL TRAINING
-    # =========================
+    
     print("ETAPE 2 - ENTRAINEMENT MODELE")
 
     model, auc, threshold, cost, cv_auc = train_model(application)
 
     print("\nETAPE 2 TERMINÉE\n")
 
-    # =========================
     # ETAPE 3 : MLFLOW
-    # =========================
+
     print("ETAPE 3 - MLFLOW")
 
     train_with_mlflow(application)
